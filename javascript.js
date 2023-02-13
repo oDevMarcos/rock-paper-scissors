6ec03c50ba43ea78cee55e527e56040984940886
Marcos <121592593+oDevMarcos@users.noreply.github.com>
2023-02-13 18:18:52 -0300

    refactor: randomChoice to arrow function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,1 @@
-function randomChoice() {
-  //make a random number between 1 2 3
-  return parseInt(Math.random() * (4 - 1) + 1);
-}
+let randomChoiceToComputer = () => parseInt(Math.random() * (4 - 1) + 1);

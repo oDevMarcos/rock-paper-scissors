8c74423c3d30204f58acf52b74a5db68a1d6610a
Marcos <121592593+oDevMarcos@users.noreply.github.com>
2023-02-13 18:30:58 -0300

    add: function computerChoice()

--- javascript.js
+++ javascript.js
@@ -1,1 +1,17 @@
 let randomChoiceToComputer = () => parseInt(Math.random() * (4 - 1) + 1);
+
+function computerChoice() {
+  switch (randomChoiceToComputer()) {
+    case 1:
+      choice = "rock";
+      break;
+    case 2:
+      choice = "paper";
+      break;
+    case 3:
+      choice = "scissors";
+      break;
+    default:
+      console.log("error");
+  }
+}
